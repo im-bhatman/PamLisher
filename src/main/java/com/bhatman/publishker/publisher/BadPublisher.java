5f76fb238e84a077fa92f4eb665e53aff61d6434
Ritesh Bhat <riteshbhat17@gmail.com>
2020-10-13 01:03:32 +0530

    initial commit - bad publisher added

---
+++ src/main/java/com/bhatman/publishker/publisher/BadPublisher.java
@@ -0,0 +1,25 @@
+package com.bhatman.publishker.publisher;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
+/**
+ * Not a great Publisher implementation, why ?
+ * No Scope of subscription.
+ * NO error event - the termination event.
+ * No backpressure compatibility.
+ */
+public class BadPublisher implements Publisher<Integer> {
+    @Override
+    public void subscribe(Subscriber<? super Integer> subscriber) {
+        int count = 0;
+
+        while (count < 20) {
+            subscriber.onNext(count);
+            count++;
+        }
+
+        subscriber.onComplete();
+
+    }
+}
